3ad23dec63725a8c6e76237f24a9eb2745f12930
tomhughes87 <tomhughes1987@gmail.com>
2022-05-07 21:31:47 +0200

    alter the final .then function to allow for multiple actions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 fetch("https://api.github.com/users/tomhughes87")
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then(function (data) {
+    console.log(data);
+  });
